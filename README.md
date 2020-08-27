5f527475a1b2d92ee0eea25143ca3c77d4ab3eb1
Bartłomiej Postek <ketsopb@gmail.com>
2020-08-27 20:05:35 +0200

    Update 0.6.5

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ![logo](https://i.imgur.com/sXnY54H.png)
-# 🍔 [763.pl](https://github.com/BarPosWasTaken/763-pl) (Free URL Shortner)
+# 🍔 [763.pl](https://github.com/BarPosWasTaken/763-pl) 0.6.5 (Free URL Shortner)
 > This is Source Code, if you want tu use [763.pl](https://github.com/BarPosWasTaken/763-pl) go here: [http://763.pl](http://763.pl)
 
 ---
@@ -31,6 +31,29 @@ Copy or Rename `.env.example` to `.env` and fill out the values:
 ```
 PORT=[PORT GOES HERE | DEFAULT 5000]
 ID_LENGTH=[SHORT ID LENGTH | DEFAULT 5]
+```
+
+Open `config/settings.json` and change the chosen values:
+
+```json
+{
+    "title": "[TITLE example: 763.pl]",
+    "url": "[URL example: http://762.pl/ <- slash at the end is required!]",
+    "images": {
+        "logo": "[SITE LOGO example: https://i.imgur.com/DEZnz6h.png]",
+        "fzf": "[404 IMAGE example: https://i.imgur.com/zsYD2OB.png]",
+        "backarrow": "[BACK-ARROW example: https://i.imgur.com/55OF6HE.png]"
+    },
+    "colors":{
+        "main":"[MAIN COLOR recommended bright color]",
+        "background":"[BACKGROUND COLOR recommended dark color]"
+    },
+    "menu":[
+        {"name":"Home", "url":"/", "target":""},
+        {"name":"Urls", "url":"/urls", "target":""},
+        {"name":"Donate", "url":"http://763.pl/aft2aR9EP", "target":"_blank"}
+    ]
+}
 ```
 
 ---
--- server.js
+++ server.js
@@ -1,49 +1,105 @@
+
+// REQUIRE
 const express = require('express');
 const mongoose = require('mongoose');
 const url = require('url'); 
 var session = require('express-session');
-const shortid = require('./id');
+var fs = require('fs');
+const shortid = require('./js/id');
 const ShortUrl = require('./models/shortUrl');
 const app = express();
 
-require('dotenv').config();
+// FILES
+const p_settings = './config/settings.json';
 
+// LOAD .ENV
+require('dotenv').config();
+//
+
+// SETUP MONGODB
 mongoose.connect('mongodb://localhost/763-pl', {
     useNewUrlParser: true, useUnifiedTopology: true
 });
 
+// FUNCTIONS
+function readJSON(path){
+    var data = JSON.parse(fs.readFileSync(path));
+    return data;
+}
+
+// READ FILES
+const settings = readJSON(p_settings);
+const menu = settings.menu;
+
+// ENGINE
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(session({secret: "jsncnd29u3-8989niun-c3nc9uiwnc9-nciw9nciu-jw9ius"}));
 
-// Index file 🛠
+// SITES
+const sites = [
+    'url',
+    'urls',
+    'short'
+];
+
+//////////////////
+// WEB REQUESTS //
+//////////////////
+
+// INDEX 🛠
 app.get('/', async (req, res) => {
     req.session.created = req.session.created;
     if(req.session.created === null){
         req.session.created = '';
     }
-    const created = req.session.created;
+    var created = req.session.created;
+    created = settings.url + created;
+    if(created == settings.url || created == settings.url + 'undefined'){
+        created = '';
+    }
     const shortUrls = await ShortUrl.find();
-    res.render('index', { shortUrls: shortUrls, created: created })
+    res.render('index', { shortUrls: shortUrls, created: created, menu: menu, settings: settings});
     req.session.created = '';
 });
 
+// LIST OF URLS
+app.get('/urls', async (req, res) => {
+    const shortUrls = await ShortUrl.find();
+    res.render('urls', { shortUrls: shortUrls, menu: menu, settings: settings});
+});
+
+// ADD TO DB REQUEST
 app.post('/url', async (req, res) => {
     var u = shortid.generate();
+    var ok = false;
+    sites.forEach(site => {
+        if(site == u) u = shortid.generate;
+    });
+    const shortUrl = await ShortUrl.find();
+    while(ok == true){
+        shortUrl.forEach(url => {
+            if(u == url) {
+                u = shortid.generate;
+                sites.forEach(s => {
+                    if(s == u){
+                        u = shortid.generate;
+                    }
+                });
+            }
+        });
+    }
+
     await ShortUrl.create({ url: req.body.fullUrl, short: u });
     req.session.created = u;
     res.redirect('/');
 });
 
-app.get('/urls', async (req, res) => {
-    const shortUrls = await ShortUrl.find();
-    res.render('urls', { shortUrls: shortUrls })
-});
-
+// OPEN URL
 app.get('/:shortUrl', async (req, res) => {
     const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
     if(shortUrl == null){
-        res.render('404', { shortUrl: shortUrl });
+        res.render('404', { shortUrl: shortUrl, menu: menu, settings: settings});
         return;
     }
 
@@ -53,4 +109,6 @@ app.get('/:shortUrl', async (req, res) => {
     res.redirect(shortUrl.url);
 });
 
+
+// LISTEN PORT
 app.listen(process.env.PORT || 5000);
